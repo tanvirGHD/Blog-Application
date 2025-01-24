65036f6a21d15a11f359ba83807a1b2718089f89
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-01-24 21:23:04 +0600

    issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
+      KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
+      KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL,
+      KINDE_SITE_URL: process.env.KINDE_SITE_URL,
+      KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+      KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+    }
+  };
+  
+  export default nextConfig;
+  
