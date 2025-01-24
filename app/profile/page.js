143600aa6ac00a16578200deb175e112390fd722
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-01-24 23:55:09 +0600

    done

--- app/profile/page.js
+++ app/profile/page.js
@@ -13,22 +13,9 @@ export default async function ProfilePage() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-6">
-      {user ? (
-        <div className="bg-white shadow-md rounded-md p-6 w-96 text-center">
-          <img
-            src={user.picture}
-            alt="Profile Picture"
-            className="w-24 h-24 rounded-full mx-auto mb-4"
-          />
-          <h2 className="text-xl font-semibold text-gray-800">
-            {user.given_name} {user.family_name}
-          </h2>
-          <p className="text-gray-600">{user.email}</p>
-        </div>
-      ) : (
-        <p className="text-red-500 text-lg">User not logged in........</p>
-      )}
+    <div>
+      <h2>Welcome to my profile</h2>
+      <p>{user.family_name}</p>
     </div>
   );
 }
